42e16bfecc3516dff9139d8d282ee215fb83c4ea
dustein <steindu@gmail.com>
2022-05-25 16:41:23 -0300

    em estudo

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+export const routes = Router();
+
+routes.get("/", (req, res) => {
+   res.send({router: "Rotas ativa - pasta raiz"})
+})
+
+routes.post("/user", (req, res) => {
+   const { email, name, apelido, telefone } = req.body;
+
+   const newUser = 
+})
+
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import express from 'express';
+import { routes } from './routes'
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3000, () => console.log("servidor ativo porta 3000"))
